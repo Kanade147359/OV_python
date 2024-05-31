81efcae0ba8f01c3f076840c3afef24cebf3d2f9
kanade147359 <kanade147359@keio.jp>
2024-05-31 15:04:53 +0900

    test.dat 時空図を作成

--- ov.py
+++ ov.py
@@ -1,23 +1,13 @@
-# python_ov_model/app.py
 
-from flask import Flask, render_template, jsonify 
 import numpy as np
 
-app = Flask(__name__)
-
 L = 40.0
-number_of_cars = 20
-cars_x = np.zeros(number_of_cars)
-cars_v = np.zeros(number_of_cars)
-c = 2.0
-a = 1.0
-dt = 0.005
+C = 2.0
 
 def V(dx):
-    return np.tanh(dx - c) + np.tanh(c)
+    return np.tanh(dx - C) + np.tanh(C)
 
-def init():
-    global cars_x, cars_v
+def init(number_of_cars, cars_x, cars_v):
     dx = L / number_of_cars
     x = 0.0
     iv = V(dx)
@@ -27,8 +17,7 @@ def init():
         x += dx
         x += (np.random.rand() - 0.5) * 0.01
 
-def step():
-    global cars_x, cars_v
+def step(number_of_cars, a , dt, cars_x, cars_v):
     for i in range(number_of_cars):
         if i != number_of_cars - 1:
             dx = cars_x[i + 1] - cars_x[i]
@@ -43,16 +32,26 @@ def step():
         cars_x[i] += cars_v[i] * dt
         if cars_x[i] > L:
             cars_x[i] -= L
+        
+def show_cars(t,number_of_cars,cars_x):
+    for i in range(number_of_cars):
+        print(f"{t} {cars_x[i]}")
+        
+def main():
+    number_of_cars = 20
+    cars_x = np.zeros(number_of_cars)
+    cars_v = np.zeros(number_of_cars)
+    a = 1.0
+    dt = 0.005
 
-@app.route('/')
-def index():
-    init()
-    return render_template('index.html')
+    init(number_of_cars, cars_x, cars_v)
 
-@app.route('/step')
-def update_step():
-    step()
-    return jsonify(cars_x=cars_x.tolist(), cars_v=cars_v.tolist())
+    for i in range(100000):
+        t = i * dt
+        step(number_of_cars, a, dt, cars_x, cars_v)
+        show_cars(t, number_of_cars, cars_x)
+
+    
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
